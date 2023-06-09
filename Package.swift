4e1d7b1dfb9888b810fbd0080ccedb0ca62a6509
Joshua <git@timesmith.ca>
2023-06-09 15:15:20 -0600

    Added SPM support

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version: 5.7
+
+import PackageDescription
+
+let package = Package(
+    name: "CocoaLumberjackSwift",
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        .library(
+            name: "CocoaLumberjackSwift",
+            targets: ["CocoaLumberjackSwiftWrapper"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/beMappy/CocoaLumberjack/", exact: "3.7.4-xc14-3"),
+    ],
+    targets: [
+        .target(
+            name: "CocoaLumberjackSwiftWrapper",
+            dependencies: [
+                .target(name: "CocoaLumberjackSwift"),
+                .product(name: "CocoaLumberjack", package: "CocoaLumberjack"),
+            ]
+        ),
+        .binaryTarget(
+            name: "CocoaLumberjackSwift",
+            path: "./CocoaLumberjackSwift.xcframework"
+        ),
+    ]
+)
---
+++ Sources/CocoaLumberjackSwiftWrapper/CocoaLumberjackSwiftWrapper.swift
@@ -0,0 +1,9 @@
+public struct CocoaLumberjackSwiftWrapper {
+    // This is required because of limited SPM support for binary products with dependencies
+    /*
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+     */
+}
